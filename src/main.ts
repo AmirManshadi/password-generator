c5f4c04a097c2336068f0336110f7b44d6c602c3
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-15 18:00:59 +0330

    feat: create variables for dom elements

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.css"
+
+// DOM elements
+const formEl = document.getElementById("password-generator-form") as HTMLFormElement
+const resultEl = document.getElementById("password-placeholder") as HTMLInputElement
+const clipboardEl = document.getElementById("copy-btn") as HTMLButtonElement
+const upperCaseEl = document.getElementById("uppercase-letters") as HTMLInputElement
+const numberEl = document.getElementById("numbers") as HTMLInputElement
+const specialEl = document.getElementById("special-characters") as HTMLInputElement
+const generateEl = document.getElementById("submit-btn") as HTMLButtonElement
