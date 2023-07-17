518a6c3029840ffc0578f5e801f6ef4edb1d32b8
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-17 19:41:49 +0330

    refactor: remove unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ const clipboardEl = document.getElementById("copy-btn") as HTMLButtonElement
 const upperCaseEl = document.getElementById("uppercase-letters") as HTMLInputElement
 const numberEl = document.getElementById("numbers") as HTMLInputElement
 const specialEl = document.getElementById("special-characters") as HTMLInputElement
-const generateEl = document.getElementById("submit-btn") as HTMLButtonElement
 
 formEl.addEventListener('submit', (e) => {
   e.preventDefault()
