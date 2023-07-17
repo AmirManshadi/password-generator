574165c744884d923237d9e63c1080249d35a796
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-17 19:20:44 +0330

    feat: create password generator class

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,45 @@ const upperCaseEl = document.getElementById("uppercase-letters") as HTMLInputEle
 const numberEl = document.getElementById("numbers") as HTMLInputElement
 const specialEl = document.getElementById("special-characters") as HTMLInputElement
 const generateEl = document.getElementById("submit-btn") as HTMLButtonElement
+
+// generator class
+class Generator {
+	static getRandomLower(): string {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+	}
+	static getRandomUpper(): string {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+	}
+	static getRandomNumber(): string {
+		return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+	}
+	static getRandomSymbol(): string {
+		const symbols = "!@#$%^&*()<>?/~"
+		return symbols[Math.floor(Math.random() * symbols.length)]
+	}
+
+	static getNecessaryFunctions(options: Generator.optionsProps): (() => string)[] {
+    let arr = [this.getRandomLower]
+    if(options.upperCase) arr.push(this.getRandomUpper)
+    if(options.numbers) arr.push(this.getRandomNumber)
+    if(options.symbols) arr.push(this.getRandomSymbol)
+    return arr
+  }
+
+	static genRandomText(len: number, options: Generator.optionsProps): string {
+    let str: string = ''
+    const functions: (() => string)[] = this.getNecessaryFunctions(options)
+    for (let i = 0; i < len; i++) {
+      str += functions[Math.floor(Math.random() * (functions.length - 1))]()
+    }
+    return str
+  }
+}
+
+namespace Generator {
+  export interface optionsProps {
+    upperCase: boolean,
+    numbers: boolean,
+    symbols: boolean
+  }
+}
