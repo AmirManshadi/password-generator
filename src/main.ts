13ef385764003475e18b479d0f516dadb678bd49
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-17 19:57:50 +0330

    feat: add option of number of characters

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ const clipboardEl = document.getElementById("copy-btn") as HTMLButtonElement
 const upperCaseEl = document.getElementById("uppercase-letters") as HTMLInputElement
 const numberEl = document.getElementById("numbers") as HTMLInputElement
 const specialEl = document.getElementById("special-characters") as HTMLInputElement
+const numberOfCharactersEl = document.getElementById("number-of-characters") as HTMLInputElement
 
-formEl.addEventListener('submit', (e) => {
+formEl.addEventListener("submit", (e) => {
   e.preventDefault()
-  resultEl.value = Generator.genRandomText(16, {
+  resultEl.value = Generator.genRandomText(+numberOfCharactersEl.value, {
     upperCase: upperCaseEl.checked,
     numbers: numberEl.checked,
     symbols: specialEl.checked
