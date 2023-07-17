cbbb1c380b54c6602fbf0e0dae5a6c44551824ae
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-07-17 19:33:12 +0330

    feat: add on submit event listener to form to gen password

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,12 @@ const upperCaseEl = document.getElementById("uppercase-letters") as HTMLInputEle
 const numberEl = document.getElementById("numbers") as HTMLInputElement
 const specialEl = document.getElementById("special-characters") as HTMLInputElement
 const generateEl = document.getElementById("submit-btn") as HTMLButtonElement
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault()
+  resultEl.value = Generator.genRandomText(16, {
+    upperCase: upperCaseEl.checked,
+    numbers: numberEl.checked,
+    symbols: specialEl.checked
+  })
+})
